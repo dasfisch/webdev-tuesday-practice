49272ec9a925ce75dd5dee647aed6c6c45799ed1
Sebastian Frohm <sebastian.frohm@gmail.com>
2024-09-03 20:16:45 -0400

    Adding in better error handling and animations

--- js/index.js
+++ js/index.js
@@ -6,9 +6,18 @@ function buttonHello(event) {
 
     let formIsValid = true;
 
-    const firstNameValue = document.getElementById('firstname').value.trim();
-    const lastNameValue = document.getElementById('lastname').value.trim();
-    const emailValue = document.getElementById('email').value.trim();
+    const firstNameField = document.getElementById('firstname');
+    const firstNameLabel = document.getElementById('firstnamelabel');
+
+    const lastNameField = document.getElementById('lastname');
+    const lastNameLabel = document.getElementById('lastnamelabel');
+
+    const emailNameField = document.getElementById('email');
+    const emailNameLabel = document.getElementById('emaillabel');
+
+    const firstNameValue = firstNameField.value.trim();
+    const lastNameValue = lastNameField.value.trim();
+    const emailValue = emailNameField.value.trim();
 
     if (firstNameValue.length === 0 || lastNameValue.length === 0 || emailValue.length === 0) {
         formIsValid = false;
@@ -17,14 +26,44 @@ function buttonHello(event) {
     if (formIsValid === false) {
         event.preventDefault();
 
-        for (let i = 0; i < labels.length; i++) {
-            labels[i].style.color = 'red';
-        }
+        // for (let i = 0; i < labels.length; i++) {
+        //     // labels[i].style.color = 'red';
+        //     labels[i].className = labels[i].className + ' error';
+        // }
+    }
+
+    if (firstNameValue.length === 0) {
+        firstNameLabel.className = firstNameLabel.className + ' error';
+    }
+
+    if (lastNameValue.length === 0) {
+        lastNameLabel.className = lastNameLabel.className + ' error';
+    }
+
+    if (emailValue.length === 0) {
+        emailNameLabel.className = emailNameLabel.className + ' error';
     }
 }
+
+const listenToTyping = (event) => {
+    const currentFieldId = event.target.id;
+
+    const label = document.getElementById(currentFieldId + 'label');
+
+    if (label.className.indexOf('error') === -1 && label.value.length === 0) {
+        label.className = label.className + ' error';
+    } else if (event.target.value.length > 0) {
+        label.className = 'label';
+    }
+};
 
 const button = document.getElementById('submit-button');
 const form = document.getElementById('contact-form');
 const labels = document.getElementsByClassName('label');
+const inputs = document.getElementsByTagName('input');
 
 form.addEventListener('submit', buttonHello);
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keypress', listenToTyping);
+}
