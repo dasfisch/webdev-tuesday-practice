e3eba8e862d167919a1ba0c00fa37744e3ddac95
Sebastian Frohm <sebastian.frohm@gmail.com>
2025-04-15 19:58:55 -0400

    Adding in form validation

--- js/array.js
+++ js/array.js
@@ -1,27 +1,24 @@
-const catPictures = [
-    "https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png",
-    "https://purepng.com/public/uploads/large/51502305566rtsfses9licuhzwzwc7hwh72e9boqm9vseph08hvvkhxgljuvmepx0w1xzaphsjwx0aftsgcwjfww6pmfytkr5pb6er0mmwugqnp.png"
-];
-
 let catPicture = '';
 
-function addCatPics()
-{
-    for(var i=0;i<catPictures.length;i++)
-    {
+//https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg
+
+function addCatPics(catPictures) {
+    // The unordered list
+    var listHolder = document.getElementById('catPics');
+    listHolder.innerHTML = '';
+
+    for (var i = 0; i < catPictures.length; i++) {
         // An array item
         catPicture = catPictures[i];
-    
+
         // An html image
-        const img = document.createElement("img");
+        const img = document.createElement('img');
         img.src = catPicture;
         img.height = 200;
         img.width = 200;
-    
-        // The unordered list
-        var listHolder = document.getElementById("catPics");
+
         // Add items to the list
-        var child = document.createElement("li");
+        var child = document.createElement('li');
         child.appendChild(img);
         listHolder.appendChild(child);
     }
--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
 console.log('hi');
 
-window.addEventListener("load", (event) => {
-    addCatPics();
+window.addEventListener('load', (event) => {
+    const catNames = [];
+
+    const catForm = document.getElementById('cat_name_form');
+    const catFormNameError = document.getElementById('cat-name-error');
+
+    catForm.addEventListener('submit', (event2) => {
+        event2.preventDefault();
+
+        const catNameField = document.getElementById('cat_name');
+
+        const catName = catNameField.value;
+
+        if (validateUrl(catName) === false) {
+            catFormNameError.classList = 'error';
+
+            return;
+        }
+
+        catFormNameError.classList = 'error hidden';
+
+        catNames.push(catName);
+
+        addCatPics(catNames);
+    });
 });
 
-document.addEventListener("click", (event) => {
-    addCatPics();
-});
+function validateUrl(value) {
+    return /^(https?:\/\/.*\.(?:png|jpg|jpeg))$/i.test(value);
+}
+
+// document.addEventListener('click', (event) => {
+//     addCatPics();
+// });
 
 
 //index.js:7 Uncaught TypeError: document.onclick is not a function
